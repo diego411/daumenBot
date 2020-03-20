d3fbb1ad1acee47a134e139a8016803798877548
yagalt <pleasemeh65@gmail.com>
2020-03-20 15:04:49 -0400

    adding restart again

--- bot.js
+++ bot.js
@@ -72,11 +72,13 @@ client.on(`chat`, (channel, tags, message, self) => {
     }
 });
 
-client.on(`chat`, (channel, tags, message, self) => {
-    if(self) return;
-    if(message === '!test') {
-    client.say(channel, `${tags.username}, dank`)
-    }
+client.on(`chat`, (channel, user, message, self) => {
+    if (message === '!restart') {
+        if(user['user-id'] != '150819483'&&user['user-id'] != '124776535') return;
+        client.say(channel, 'restarting').then(() => {
+        process.exit(1);
+      })
+      };
 });
 
 client.on(`chat`, (channel, tags, message, self) => {
