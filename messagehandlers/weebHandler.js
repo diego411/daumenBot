5490f9e88bfc64e4000f51dd44d4b49a3b31bd44
diego411 <diego.enrico@directbox.com>
2022-03-10 21:28:55 +0100

    test

--- messagehandlers/weebHandler.js
+++ messagehandlers/weebHandler.js
@@ -15,7 +15,7 @@ const handle = (msg, client) => {
     if (msg.displayName === 'daumenbot') return;
     if (msg.channelName != 'forsen') {
         if (msg.messageText.includes("daumenbot") || weebC % 25 === 0) {
-            if (msg.senderUserID === '275711366'|| msg.senderUserID === '455288756') return;
+            if (msg.senderUserID === '275711366' || msg.senderUserID === '455288756') return;
             let rand = Math.floor(Math.random() * 3)
             if (rand == 0) client.say(msg.channelName, `${msg.senderUsername}, NaM stfu`);
             if (rand == 1) client.say(msg.channelName, `${msg.senderUsername}, NaM 🇻🇳 ⣰⠛⣦⠛⣿⠛⢸⠛⠛⣿⠀⣿⠀⠸⡇⣸⡄⡿⢸⠛⠛⣿⠛⠃⣿⠛⡆⣴⠛⣦⠀ ⠘⠷⣄⠀⣿⠀⢸⠶⠆⣿⠀⣿⠀⠀⣇⡇⣇⡇⢸⠶⠆⣿⠶⠆⣿⠾⡅⠙⠶⣄⠀ ⠻⣤⠟⠀⠿⠀⠸⠀⠀⠹⣤⠟⠀⠀⠹⠃⠻⠀⠸⠤⠤⠿⠤⠄⠿⠤⠇⠻⣤⠟ `)
@@ -24,14 +24,15 @@ const handle = (msg, client) => {
         }
         weebC++;
     } else {
-        if (puke % 1000 === 0&&weebcd.fire()) {
+        if (puke % 1000 === 0 && weebcd.fire()) {
             let rand = Math.floor(Math.random() * 3)
             if (rand == 0) client.say(msg.channelName, `${msg.senderUsername}, NaM stfu`);
             if (rand == 1) client.say(msg.channelName, `${msg.senderUsername}, NaM 🇻🇳 ⣰⠛⣦⠛⣿⠛⢸⠛⠛⣿⠀⣿⠀⠸⡇⣸⡄⡿⢸⠛⠛⣿⠛⠃⣿⠛⡆⣴⠛⣦⠀ ⠘⠷⣄⠀⣿⠀⢸⠶⠆⣿⠀⣿⠀⠀⣇⡇⣇⡇⢸⠶⠆⣿⠶⠆⣿⠾⡅⠙⠶⣄⠀ ⠻⣤⠟⠀⠿⠀⠸⠀⠀⠹⣤⠟⠀⠀⠹⠃⠻⠀⠸⠤⠤⠿⠤⠄⠿⠤⠇⠻⣤⠟ `)
             if (rand == 2) client.say(msg.channelName, `${msg.senderUsername}, NaM stfu weeb`);
             logger.log(`NaMed ${msg.senderUsername} for weeb term: ${latestWeebTerm} in ${msg.channelName}`)
         }
-        db.get('forsenPuke').then(puke => db.set('forsenPuke', puke +1))
+        db.get('forsenPuke').then(puke => db.set('forsenPuke', puke + 1))
+        puke++;
     }
 }
 
