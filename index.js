b5c3c2762ac291b404a6c93a376f4bd05b547ae4
diego411 <diego.enrico@directbox.com>
2022-03-10 21:36:18 +0100

    test xd 2

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ weebHandler.init(db)
 
 const client = require('./client');
 
-logger.log(process.env["OAUTH"])
 if (process.env.NODE_ENV !== 'production') db.get('debugchannels').then(client.init)
 else db.get('channels').then(client.init)
 
