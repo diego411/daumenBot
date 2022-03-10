ed015e973a1b216a08df79bf69d5bdfc2dbc5c90
diego411 <diego.enrico@directbox.com>
2022-03-10 22:02:05 +0100

    danl

--- client.js
+++ client.js
@@ -19,6 +19,7 @@ const cd = new cooldown(2000)
 //const pyramidcd = new cooldown(15000)
 
 const init = (channels) => {
+    console.log(channels)
     client.connect()
     client.joinAll(channels)
     for (let j = 0; j < channels.length; j++) logger.log(`joined ${channels[j]}`)
