3b00168f920d9042e05ca346ad2700767b64661b
diego411 <diego.enrico@directbox.com>
2022-05-10 23:47:52 +0200

    add wedcontroller and weebcheck command

---
+++ controllers/wed.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+
+const WED_BASE_URL = process.env["WED_BASE_URL"] || "http://localhost:5000/api/v1/"
+
+exports.weebCheck = async (channel, message) => {
+    let response
+    try {
+        response = await axios({
+            method: 'get',
+            url: `${WED_BASE_URL}hwis`,
+            headers: { "Content-Type": "application/json" },
+            data: { channel: channel, message: message }
+        })
+        return response.data
+    } catch (e) {
+        console.log(e)
+        return null
+    }
+}
