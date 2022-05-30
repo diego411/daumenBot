8c42f41e77ec6022ac72fa7eef63f87aff739b8d
Brian <18603393+brian6932@users.noreply.github.com>
2022-05-30 18:53:02 -0400

    lol

--- utils/userFormatter.js
+++ utils/userFormatter.js
@@ -1,4 +1,4 @@
 module.exports = function formatUser(inputtedUser) {
-        inputtedUser = /(?<=\b|_)[a-z0-9]\w{0,24}\b/i.exec(inputtedUser)
-        return inputtedUser
+    inputtedUser = /(?<=\b|_)[a-z0-9]\w{0,24}\b/i.exec(inputtedUser)
+    return inputtedUser
 }
