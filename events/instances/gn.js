26054e59f63a51e742f3464fbde4aedb2361941d
diego411 <diego.enrico@directbox.com>
2022-06-09 16:20:44 +0200

    fix brian pr smh

--- events/instances/gn.js
+++ events/instances/gn.js
@@ -2,5 +2,5 @@ module.exports = {
     name: "gn",
     arg_flags: ["sender_name"],
     code: ({ senderUsername }) => `$tuck ${senderUsername} Hope you have a good night Foreheadkiss ❤`,
-    trigger: (msg) => msg.channelName === 'eiectricevil' && /^$gn(?:\s|$)/i.test(msg.messageText)
+    trigger: (msg) => msg.channelName === 'eiectricevil' && /^\$gn(?:\s|$)/i.test(msg.messageText)
 }
