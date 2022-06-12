6685b9c4d5885abdd368f1d0253af6ee2095bac2
diego411 <diego.enrico@directbox.com>
2022-06-13 00:42:42 +0200

    improve rwl responses

--- commands/instances/randomweebline.js
+++ commands/instances/randomweebline.js
@@ -1,4 +1,5 @@
-const WLoggerController = require('../../controllers/wlogger')
+const wLoggerController = require('../../controllers/wlogger')
+const twitchController = require('../../controllers/twitch')
 const timeUtil = require('../../utils/time')
 const formatUser = require('../../utils/formatter').formatUser
 
@@ -12,15 +13,18 @@ module.exports = {
             channel_name = channelName
         else channel_name = formatUser(raw_args[0])
 
-        if (!channel_name)
+        if (!channel_name || !twitchController.getUserId(channel_name))
             return `Invalid channel provided`
 
-        const stats = await WLoggerController.statsForChannel(channel_name)
+        const stats = await wLoggerController.statsForChannel(channel_name)
         if (!stats) return `Encountered problem with wlogger api`
+
+        if (!stats.exists)
+            return `I never logged any messages in #${channel_name} NaM`
 
         const randomLine = stats.messages[Math.floor(Math.random() * stats.messages.length)]
 
-        if (stats.message_count == 0) return `I have no weeb line logged in this channel NaM 👍`
+        if (stats.message_count == 0) return `I have no weeb line logged in #${channel_name} NaM 👍`
         return `(${timeUtil.relativeTime(randomLine.post_timestamp * 1000)} ago) ${randomLine.sender_login}: ${randomLine.content}`
     },
     trigger: (command_name) => command_name === "randomweebline" || command_name === "rwl",
